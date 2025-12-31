5e49bbc5984340b28af149c0da36e2248aa18a54
JamesPersonalCode56 <lgm05062002@gmail.com>
2025-12-31 08:29:50 +0000

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+if [ ! -d ".venv" ]; then
+  python3 -m venv .venv
+fi
+
+. .venv/bin/activate
+
+if ! command -v poetry >/dev/null 2>&1; then
+  python -m pip install --upgrade pip
+  python -m pip install poetry
+fi
+
+poetry install
+
 cd src
 poetry run connector-wss --host 0.0.0.0 --port 8765
